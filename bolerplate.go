ac3c4d04d0e26264fce58158ce8bc1349c11bc7e
urbanroman <romans.urbans-orbans@printful.com>
2022-12-04 12:52:52 +0200

    day4 part1

---
+++ bolerplate.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input_data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		fmt.Println(line)
+	}
+}
---
+++ day4/camp_cleanup.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// [1-99] array of all camp sections
+	var campSections = [99]int{}
+	for i := 0; i < len(campSections); i++ {
+		campSections[i] = i + 1
+	}
+
+	file, err := os.Open("input_data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	pairsWithRangeIntersect := 0
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		groupRanges := strings.Split(line, ",")
+		groupFrstRangeBoundaries := strings.Split(groupRanges[0], "-")
+		groupScndRangeBoundaries := strings.Split(groupRanges[1], "-")
+		groupFrstRangeBoundaryL, _ := strconv.Atoi(groupFrstRangeBoundaries[0])
+		groupFrstRangeBoundaryH, _ := strconv.Atoi(groupFrstRangeBoundaries[1])
+		groupScndRangeBoundaryL, _ := strconv.Atoi(groupScndRangeBoundaries[0])
+		groupScndRangeBoundaryH, _ := strconv.Atoi(groupScndRangeBoundaries[1])
+		groupFrstSlice := campSections[groupFrstRangeBoundaryL-1 : groupFrstRangeBoundaryH]
+		groupScndSlice := campSections[groupScndRangeBoundaryL-1 : groupScndRangeBoundaryH]
+
+		intersect := Intersection(groupFrstSlice, groupScndSlice)
+
+		if len(intersect) == len(groupFrstSlice) || len(intersect) == len(groupScndSlice) {
+			pairsWithRangeIntersect++
+		}
+		fmt.Println(line)
+		fmt.Println(groupFrstSlice, groupScndSlice)
+	}
+
+	fmt.Println(pairsWithRangeIntersect)
+}
+
+func Intersection(a, b []int) (c []int) {
+	m := make(map[int]bool)
+
+	for _, item := range a {
+		m[item] = true
+	}
+
+	for _, item := range b {
+		if _, ok := m[item]; ok {
+			c = append(c, item)
+		}
+	}
+	return
+}
